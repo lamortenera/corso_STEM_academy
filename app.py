197f266c6cce98166e9acb21df821ad8d6666022
Alessandro Mammana <ale.fener@gmail.com>
2019-06-10 21:09:51 +0200

    Aggiunta del grafico

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ Created on Sun Jun  2 11:24:03 2019
 @author: ale
 """
 
-from utils import read_data
+import datetime
+from utils import read_data, data_to_chart, datetime_to_str
 from flask import Flask, Markup, render_template
 
 def get_table_content(data):
@@ -20,7 +21,10 @@ app = Flask(__name__)
 def index():
     data = read_data()
     table_content = get_table_content(data)
-    return render_template("index.html", table_content=Markup(table_content))
+    data_to_chart(data)
+    now_str = datetime_to_str(datetime.datetime.now())
+    return render_template("index.html", 
+        table_content=Markup(table_content), now_str=now_str)
 
 @app.route("/authors")
 def authors():
