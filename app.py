2496e2b5b0380d197c7e7584ffb058326e4aac15
Alessandro Mammana <ale.fener@gmail.com>
2019-06-10 20:33:15 +0200

    Aggiunta della tabella

--- app.py
+++ app.py
@@ -5,12 +5,22 @@ Created on Sun Jun  2 11:24:03 2019
 @author: ale
 """
 
-from flask import Flask, render_template
+from utils import read_data
+from flask import Flask, Markup, render_template
+
+def get_table_content(data):
+    content = ""
+    row_pattern = "<tr><td>{}</td><td>{}</td><td>{:.2f}</td></tr>"
+    for row in data:
+        content += row_pattern.format(row["datetime"], row["city"], row["temperature"])
+    return content
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data = read_data()
+    table_content = get_table_content(data)
+    return render_template("index.html", table_content=Markup(table_content))
 
 @app.route("/authors")
 def authors():
